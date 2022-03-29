37e2293b0f8a98869a6105aa9dc17f953114b30b
miles <miles4599@gmail.com>
2022-03-29 13:44:21 -0700

    game is working

--- war.py
+++ war.py
@@ -1,5 +1,9 @@
 import random
 
+turn_count = 0
+war_count = 0
+game_over = False
+
 
 class Deck:
     def __init__(self):
@@ -7,8 +11,8 @@ class Deck:
         self.build()
 
     def build(self):
-        for s in ["Spades", "Clubs", "Diamonds", "hearts"]:
-            for v in range(1, 14):
+        for s in ["Sp", "Cl", "Di", "He"]:
+            for v in range(2, 15):
                 self.cards.append(Card(s, v))
 
     def show(self):
@@ -28,46 +32,103 @@ class Card:
     def __init__(self, suit, val):
         self.suit = suit
         self.value = val
+        self.isBattleCard = False
+
+    def __str__(self):
+        return "{}-{}, ".format(self.value, self.suit)
 
     def show(self):
-        print("{} of {}".format(self.value, self.suit))
+        print("{}-{}, ".format(self.value, self.suit), end="")
 
 
 class Player:
     def __init__(self, name):
         self.name = name
         self.hand = []
+        self.battle_card = None
 
     def draw(self, deck):
         self.hand.append(deck.draw_card())
         return self
 
     def showHand(self):
+        print("{} has {} cards: ".format(self.name, len(self.hand)))
+        i = 0
         for card in self.hand:
+            i += 1
+            if i % 10 == 0:
+                print()
             card.show()
+        print()
 
 
-class WarGame:
-    def __init__(self):
-        self.p1 = Player("Player-1")
-        self.p2 = Player("Player-2")
-        self.deck = Deck()
-        self.deck.shuffle()
-        self.deal()
-
-    def deal(self):
-        while len(self.deck.cards) > 0:
-            self.p1.hand.append(self.deck.cards.pop())
-            if len(self.deck.cards) > 0:
-                self.p2.hand.append(self.deck.cards.pop())
-
-    def print_cards(self):
-        print("Player-1 cards: ")
-        self.p1.showHand()
-        print("\n")
-        print("Player-2 cards: ")
-        self.p2.showHand()
+def dealCards(p1, p2, deck):
+    deck.shuffle()
+    while len(deck.cards) > 0:
+        p1.hand.append(deck.draw_card())
+        if len(deck.cards) > 0:
+            p2.hand.append(deck.draw_card())
 
 
-game = WarGame()
-game.print_cards()
+def gameTurn(p1, p2, table_cards):
+
+    global turn_count
+    global war_count
+    global game_over
+    turn_count += 1
+    if len(p1.hand) < 1 or len(p2.hand) < 1:
+        game_over = True
+        return
+    # take out of hand and call it a battle card
+    p1.battle_card = p1.hand.pop()
+    p2.battle_card = p2.hand.pop()
+    # put battle card on the table
+    table_cards.append(p1.battle_card)
+    table_cards.append(p2.battle_card)
+    print(f"{p1.battle_card} vs {p2.battle_card}")
+    for c in table_cards:
+        print(c)
+    card1 = p1.battle_card.value
+    card2 = p2.battle_card.value
+
+    # go to war
+    if card1 == card2:
+        war_count += 1
+        if len(p1.hand) < 4 or len(p2.hand) < 4:
+            game_over = True
+            return
+        table_cards.append(p1.hand.pop())
+        table_cards.append(p1.hand.pop())
+        table_cards.append(p1.hand.pop())
+
+        table_cards.append(p2.hand.pop())
+        table_cards.append(p2.hand.pop())
+        table_cards.append(p2.hand.pop())
+
+    elif card1 < card2:
+        for c in table_cards:
+            p2.hand.insert(0, c)
+        table_cards.clear()
+    else:
+        for c in table_cards:
+            p1.hand.insert(0, c)
+        table_cards.clear()
+
+
+def play_game(p1, p2, table_cards):
+    dealCards(p1, p2, deck)
+    p1.showHand()
+    p2.showHand()
+    while game_over != True:
+        print(f"\n***** turn {turn_count} *****")
+        gameTurn(p1, p2, table_cards)
+        p1.showHand()
+        p2.showHand()
+    print(f"War count: {war_count}")
+
+
+table_cards = []
+p1 = Player("Player 1")
+p2 = Player("Player 2")
+deck = Deck()
+play_game(p1, p2, table_cards)
